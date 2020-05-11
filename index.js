7310a4fde1ce23c3e2ca7074f77ae62d25c5e148
Callum Alton <callum.alton33@gmail.com>
2020-05-11 10:03:29 +0100

    finishing up box challeneg (still need search)

--- index.js
+++ index.js
@@ -4,27 +4,29 @@ const clearbtn = document.getElementById('clearButton')
 
 // adds boxes
 
-let numBoxes // let variable for number of boxes
-btn.onclick = () => {
 
-    numBoxes = input.value
+btn.onclick = () => { // .onclick means when the button is clicked
+
+    let numBoxes = number.value
+    let imageSearch = input.value
 
 for (i = 0; i < numBoxes; i++) {
-
     const box = document.createElement('box')
     box.className = 'box'
 
-    let generatedImage = document.createElement('img')
-    generatedImage.src = "5bf43911110d4c1bb210b93c.jpeg"
-    generatedImage.setAttribute("height", "255vw / 3.5")
-    generatedImage.setAttribute("width", "255vw / 3.5")
+    let generatedImage = document.createElement('img') 
+
+    let numGen = Math.ceil((Math.random() * 100) + 1); // randomises the image
+
+    generatedImage.src = `https://source.unsplash.com/random/500x500/?sig=${imageSearch}&${numGen}`
+    generatedImage.setAttribute("height", "425vw / 3.5")
+    generatedImage.setAttribute("width", "425vw / 3.5")
     
     wrapper.appendChild(box)
     box.appendChild(generatedImage)
-
-    console.log(numBoxes)
-    }
 }
+}
+
 
 // clears all boxes
 
@@ -33,3 +35,12 @@ clearbtn.onclick = () => {
     wrapper.removeChild(wrapper.firstChild)
     }
 
+// The removeChild() method removes a specified child node of the specified element.
+// Returns the removed node as a Node object, or null if the node does not exist.
+
+// The Node.firstChild read-only property returns the node's first child in the tree, or null if the node has no children. 
+// If the node is a Document, it returns the first node in the list of its direct children.
+
+// The Node.lastChild read-only property returns the last child of the node. 
+// If its parent is an element, then the child is generally an element node, a text node, or a comment node. 
+// It returns null if there are no child elements.
